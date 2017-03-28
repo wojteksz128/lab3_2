d3ce336a754e6a7996119c38923841c9274c0d91
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-28 18:04:53 +0200

    Prepare test configuration. Add stub ConfigurationLoader object.

--- src/test/java/NewsLoaderTest.java
+++ src/test/java/NewsLoaderTest.java
@@ -1,5 +1,24 @@
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+
 /**
  * Created by Wojciech Szczepaniak on 28.03.2017.
  */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
 public class NewsLoaderTest {
+
+    @Before
+    public void test() {
+        mockStatic(ConfigurationLoader.class, NewsReaderFactory.class);
+        ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+    }
 }
